058c1bfca0888cbc0b881274a98af816dee19934
Soenke Hack <soenke.hack@leanix.net>
2021-01-28 11:30:40 +0100

    INTG-1802 Retrieve secrets in parallel

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ const { SecretClient } = require("@azure/keyvault-secrets");
         const credentials = JSON.parse(buffer.toString());
         const keyvaultClient = new SecretClient(credentials.secretStoreAddress, new ClientSecretCredential(credentials.tenantId, credentials.clientId, credentials.clientSecret));
         console.log("Starting to look for secrets in secret-store", credentials.secretStoreAddress);
-        for await (let secretProperties of keyvaultClient.listPropertiesOfSecrets()) {
+        await Promise.all(keyvaultClient.listPropertiesOfSecrets().map(async (secretProperties) => {
             const secretName = secretProperties.name;
             console.log("Found secret", secretName);
-            const { value: latestSecret } = await keyvaultClient.getSecret(secretName);
+            return { secretName: secretName, latestSecret: await keyvaultClient.getSecret(secretName) };
+        })).then((secretName, latestSecret) => {
             core.setSecret(latestSecret)
             const secretEnvName = secretName.split(" ").join("_").split("-").join("_").toUpperCase()
             console.log("Setting environment variable", secretEnvName)
             core.exportVariable(secretEnvName, latestSecret);
-        }
+        })
     } catch (error) {
         core.setFailed(error.message);
     }
